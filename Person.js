b915c61ed3f344a000ed3a9213a61cfa3763ef5f
Jahnavi Yadav <84310089+Jahnavi-Yadav@users.noreply.github.com>
2023-03-01 13:29:38 +0530

    Add files via upload

---
+++ Person.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Person extends Component{
+    render(){
+        return(
+            <div>
+            <div>Name: Jahnavi</div>
+            <div>Age: 20 Years</div>
+            </div>
+        );
+    }
+
+}
+export default Person;
